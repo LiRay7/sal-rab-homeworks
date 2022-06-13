0821f9511af52f98c34d4597329e4008950c94c0
Anna Li <a.satory@gmail.com>
2022-06-13 20:21:34 +0300

    1st try, title undefined

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,20 +34,27 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
-
-    let countOfGoods = goods.length;
-
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
+    let data = {
+        client: `${name + ' ' + phone}`, 
+        goods: [], 
+        order: {address: address, sum: sum}
+    };
+    
+        let countOfGoods = goods.length;
+        
+     for (let i = 0; i <= countOfGoods; i++) {
+        data.goods.push({title: goods[i].title, count: goods[i].count});
     }
-
+   
+     
     data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
+    data.order.sum = sum;
+    
+    //data.order.sum = name + phone + address + goods + sum;
+    //data.client = 'Иван';
 
-    data.client = 'Иван';
-
-    let jsonData = JSON.stringify(data);
+    let jsonData = JSON.stringify({data});
 
     return jsonData;
 }
+
